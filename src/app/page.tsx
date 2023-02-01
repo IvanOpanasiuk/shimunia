598f1f31ea44d16035b0cc6a8bc6c18bbb5580bc
Iva Opanasiuk <vanjoker88@gmail.com>
2023-02-01 13:24:07 +0200

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>It's my site and blog</p>
+        <p>It is my site and blog</p>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
